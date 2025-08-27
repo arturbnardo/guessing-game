7f9adf9f42f59333f7ba8cef7a4a2c24425fced7
Artur Bernardo <arturbrnd1@gmail.com>
2025-08-27 17:44:44 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import styles from "./app.module.css"
+
+import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
+import { Header } from "./components/Header"
+
+export default function App() {
+  
+  function handleRestartGame(){
+    alert("Reiniciar o jogo!")
+  }
+  
+  return (
+    <div className={styles.container}>
+      <main>
+      <Header current={5} max={10} onRestart={handleRestartGame} />
+      
+      <Tip tip="Uma das linguagens de programação mais utilizadas" />
+
+      <Letter value="R"/>
+      </main>
+    </div>
+  )
+}
