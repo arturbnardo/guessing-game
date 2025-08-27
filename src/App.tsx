737634002ca9ef2c6db135a1d05dca035a65a98f
Artur Bernardo <arturbrnd1@gmail.com>
2025-08-27 20:20:27 -0300

    letters used style

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Input } from "./components/Input"
 import { Button } from "./components/Button"
 import { Letter } from "./components/Letter"
 import { Header } from "./components/Header"
+import { LettersUsed } from "./components/LettersUsed"
 
 export default function App() {
   
@@ -33,6 +34,8 @@ export default function App() {
         <Input autoFocus maxLength={1} placeholder="?"/>
         <Button title="Confirmar" />
       </div>
+
+      <LettersUsed />
       </main>
     </div>
   )
